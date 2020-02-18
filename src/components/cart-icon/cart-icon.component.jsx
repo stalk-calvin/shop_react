c450458deb2f805ff5ae19fd672dda9afb006690
stalk-calvin <stalk.calvin@gmail.com>
2020-02-17 16:38:42 -0800

    Adding item count / memoization

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { toggle } from '../../redux/toggle/toggle.action';
+import { cartAction } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggle }) => (
+const CartIcon = ({ toggle, itemCount }) => (
     <div className='cart-icon' onClick={toggle}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{ itemCount }</span>
     </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-    toggle: () => dispatch(toggle())
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapDispatchToProps = dispatch => ({
+    toggle: () => dispatch(cartAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.styles.scss';
 
 const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
     <button 
-        className={`${inverted ? 'inverted' : ' ' }${isGoogleSignIn ? 'google-sign-in' : ' ' }custom-button`} 
+        className={`${inverted ? 'inverted ' : '' }${isGoogleSignIn ? 'google-sign-in ' : '' }custom-button`} 
         {...otherProps}>
         {children}
     </button>
